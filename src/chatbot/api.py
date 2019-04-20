d8a9db54cd4dda8d327aec428a189cd8770f633c
Mehmet Alp Sümer <mehmetalpsumer@gmail.com>
2019-04-20 19:05:12 +0300

    Add chat ui

--- src/chatbot/api.py
+++ src/chatbot/api.py
@@ -1,7 +1,16 @@
 import json
+import nltk
+import numpy as np
+import random
+import string
 from django.http import JsonResponse
 
 from .models import UserMessage, BotMessage
+
+
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
+
+GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
 
 
 def message(request):
@@ -10,6 +19,7 @@ def message(request):
     and returns the response of the chat bot.
     '''
     if request.method == 'POST':
+        # Read and save user message
         incoming_data = json.loads(request.body)
         user_message = incoming_data['userMessage']
         user_message_obj = UserMessage.create(
@@ -18,7 +28,12 @@ def message(request):
         )
         user_message_obj.save()
 
-        bot_message = 'Test'
+        # Create bot message
+        user_message = user_message.lower()
+
+        bot_message = 'This message is from the backend.'
+
+        # Create bot message object, save and send
         bot_message_obj = BotMessage.create(
             response_to=user_message_obj,
             message=bot_message
